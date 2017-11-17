f5246ece9f38e15152d201c4811f3a503dc17180
wlrllr <398459101@qq.com>
2017-11-17 18:30:56 +0800

    提交

--- src/main/java/com/wlrllr/sdk/core/intercepter/CustomUrlInterceptor.java
+++ src/main/java/com/wlrllr/sdk/core/intercepter/CustomUrlInterceptor.java
@@ -1,5 +1,6 @@
 package com.wlrllr.sdk.core.intercepter;
 
+import com.wlrllr.sdk.core.AbstractHandleAdapter;
 import com.wlrllr.sdk.core.AbstractHandler;
 import com.wlrllr.sdk.util.SpringUtils;
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -10,6 +11,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Objects;
 
 /**
  * Created by wlrllr on 2017/11/17.
@@ -20,18 +22,29 @@ public class CustomUrlInterceptor {
 
     private static Logger logger = LoggerFactory.getLogger(CustomUrlInterceptor.class);
 
-    @Around("execution(* *..*.customerServerUrl(..))")
+    @Around("execution(* *..*.index(..))")
     public Object method(ProceedingJoinPoint proceedingJoinPoint) {
         logger.info(">>>>>>进入拦截器<<<<<<<<");
         try {
-            Class clazz = proceedingJoinPoint.getThis().getClass().getSuperclass();
+            Class clazz = proceedingJoinPoint.getThis().getClass();
             Object[] args = proceedingJoinPoint.getArgs();
-            Class parent = clazz.getSuperclass().getSuperclass();
-            if (parent == null || !parent.isAssignableFrom(AbstractHandler.class)) {
-                return proceedingJoinPoint.proceed();
+            Class parent = clazz.getSuperclass();
+            Boolean interceptorFlag = false;
+            while(true){
+                if(parent.isAssignableFrom(Object.class)){
+                    break;
+                }
+                if(parent.isAssignableFrom(AbstractHandleAdapter.class)){
+                    interceptorFlag = true;
+                    break;
+                }
+                parent = parent.getSuperclass();
             }
-            AbstractHandler handler = SpringUtils.getBean(AbstractHandler.class);
-            return handler.index((HttpServletRequest)args[0]);
+            if (interceptorFlag) {
+                AbstractHandler handler = SpringUtils.getBean(AbstractHandler.class);
+                return handler.index((HttpServletRequest)args[0]);
+            }
+            return proceedingJoinPoint.proceed();
         } catch (Exception e) {
           e.printStackTrace();
         } catch (Throwable throwable) {
